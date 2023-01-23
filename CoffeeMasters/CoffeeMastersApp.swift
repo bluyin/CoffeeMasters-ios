cd75eb768e9ef4e417ed0b91d1440908cd380b4f
Bluyin <juanillo11211@gmail.com>
2023-01-23 20:37:18 +0100

    Initial Commit

---
+++ CoffeeMasters/CoffeeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMastersApp.swift
+//  CoffeeMasters
+//
+//  Created by Bluyin  on 23/1/23.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
